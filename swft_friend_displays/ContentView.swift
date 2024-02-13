443f04ad04a90484b5926e95e2f75a9e535883b2
Rahul Shah <rahultshah@gmail.com>
2024-02-13 00:37:44 -0600

    Initial Commit

---
+++ swft_friend_displays/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swft_friend_displays
+//
+//  Created by Rahul Shah on 2/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swft_friend_displays/swft_friend_displaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  swft_friend_displaysApp.swift
+//  swft_friend_displays
+//
+//  Created by Rahul Shah on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct swft_friend_displaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
